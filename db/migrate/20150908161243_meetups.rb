234f9bb0529a52ad2acaaeac6810a081595bf6ef
Michelle Casey <mcasey5216>
2015-09-08 15:42:39 -0400

    initial commit

---
+++ db/migrate/20150908161243_meetups.rb
@@ -0,0 +1,11 @@
+class Meetups < ActiveRecord::Migration
+  def change
+    create_table :meetups do |table|
+      table.string :name, null: false
+      table.string :description, null: false
+      table.string :location, null: false
+
+      table.timestamps
+    end
+  end
+end
